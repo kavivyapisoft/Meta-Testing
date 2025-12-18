13033d4d07df2645670a2f2726344c6656299d8e
kavivyapisoft <kavi@vyapisoft.com>
2025-12-18 09:51:03 +0530

    Bug fix

--- app.js
+++ app.js
@@ -64,56 +64,56 @@ app.post('/webhook', async (req, res) => {
       if (messages.text.body.toLowerCase() === 'buttons') {
         sendReplyButtons(messages.from)
       }
-      if(messages.text.body){
-          let regexMap = {
-            request_btn: /^REQ-\d{4}$/,
-            hp_btn: /^HP-\d{4}$/,
-            nci_btn: /^NCI-\d{6}-\d{4}$/
-          };   
-          let buttonId = messages.interactive.button_reply.id;
-          let selectedRegex = regexMap[buttonId];
-          let userInput = messages.text.body.trim();
-          let errorMessageMap = {
-                request_btn: '❌ Invalid Request ID. Use format: REQ-1234',
-                hp_btn: '❌ Invalid Hot Part ID. Use format: HP-1234',
-                nci_btn: '❌ Invalid NCI ID. Use format: NCI-202512-1234'
-              };
-          let successMessageMap = {
-                request_btn: '✅ Thank you! Your Request ID has been verified successfully.',
-                hp_btn: '✅ Thank you! Your Hot Part ID has been verified successfully.',
-                nci_btn: '✅ Thank you! Your NCI number has been verified successfully.'
-              };
+      // if(messages.text.body){
+      //     let regexMap = {
+      //       request_btn: /^REQ-\d{4}$/,
+      //       hp_btn: /^HP-\d{4}$/,
+      //       nci_btn: /^NCI-\d{6}-\d{4}$/
+      //     };   
+      //     let buttonId = messages.interactive.button_reply.id;
+      //     let selectedRegex = regexMap[buttonId];
+      //     let userInput = messages.text.body.trim();
+      //     let errorMessageMap = {
+      //           request_btn: '❌ Invalid Request ID. Use format: REQ-1234',
+      //           hp_btn: '❌ Invalid Hot Part ID. Use format: HP-1234',
+      //           nci_btn: '❌ Invalid NCI ID. Use format: NCI-202512-1234'
+      //         };
+      //     let successMessageMap = {
+      //           request_btn: '✅ Thank you! Your Request ID has been verified successfully.',
+      //           hp_btn: '✅ Thank you! Your Hot Part ID has been verified successfully.',
+      //           nci_btn: '✅ Thank you! Your NCI number has been verified successfully.'
+      //         };
 
-          if (!selectedRegex.test(userInput)) {
+      //     if (!selectedRegex.test(userInput)) {
 
-              // if (buttonId === 'request_btn') {
-              //   sendMessage(
-              //     messages.from,
-              //     '✅ Thank you! Your Request ID has been verified successfully.'
-              //   );
-              // } else if (buttonId === 'hp_btn') {
-              //   sendMessage(
-              //     messages.from,
-              //     '✅ Thank you! Your Hot Part ID has been verified successfully.'
-              //   );
-              // } else if (buttonId === 'nci_btn') {
-              //   sendMessage(
-              //     messages.from,
-              //     '✅ Thank you! Your NCI number has been verified successfully.'
-              //   );
-              // } 
-              sendMessage(
-                messages.from,
-                successMessageMap[buttonId]
-              );
+      //         // if (buttonId === 'request_btn') {
+      //         //   sendMessage(
+      //         //     messages.from,
+      //         //     '✅ Thank you! Your Request ID has been verified successfully.'
+      //         //   );
+      //         // } else if (buttonId === 'hp_btn') {
+      //         //   sendMessage(
+      //         //     messages.from,
+      //         //     '✅ Thank you! Your Hot Part ID has been verified successfully.'
+      //         //   );
+      //         // } else if (buttonId === 'nci_btn') {
+      //         //   sendMessage(
+      //         //     messages.from,
+      //         //     '✅ Thank you! Your NCI number has been verified successfully.'
+      //         //   );
+      //         // } 
+      //         sendMessage(
+      //           messages.from,
+      //           successMessageMap[buttonId]
+      //         );
           
-          } else {
-              sendMessage(
-                messages.from,
-                errorMessageMap[buttonId]
-              );
-            }
-      }
+      //     } else {
+      //         sendMessage(
+      //           messages.from,
+      //           errorMessageMap[buttonId]
+      //         );
+      //       }
+      // }
     }
 
     if (messages.type === 'interactive') {
@@ -124,38 +124,32 @@ app.post('/webhook', async (req, res) => {
 
      if (messages?.interactive?.type === 'button_reply') {
 
-        let buttonId = messages.interactive.button_reply.id;
-        let buttonTitle = messages.interactive.button_reply.title;
-
-        console.log(messages.interactive.button_reply);
-        console.log(buttonId,buttonTitle);
-        
-            if (buttonId === 'request_btn') {
+            if (messages.interactive.button_reply.id === 'request_btn') {
 
               sendMessage(
                 messages.from,
-                '📝 You selected REQUEST. Please enter your Request ID (example: REQ-1234)'
+                '📝 Please enter your Request ID (example: REQ-1234)'
               );
 
-            } else if (buttonId === 'hp_btn') {
+            } else if (messages.interactive.button_reply.id === 'hp_btn') {
 
               sendMessage(
                 messages.from,
-                '📝 You selected REQUEST. Please enter your Request ID. (example: HP-1234)'
+                '📝 Please enter your Request ID. (example: HP-1234)'
               );
 
-            } else if (buttonId === 'nci_btn') {
+            } else if (messages.interactive.button_reply.id === 'nci_btn') {
 
               sendMessage(
                 messages.from,
-                '📝 You selected REQUEST. Please enter your Request ID. (example: NCI-202512-1234)'
+                '📝 Please enter your Request ID. (example: NCI-202512-1234)'
               );
 
             } else {
 
               sendMessage(
                 messages.from,
-                `You selected: ${buttonTitle}`
+                `You selected: ${messages.interactive.button_reply.title}`
               );
             }
       }
